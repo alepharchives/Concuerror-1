1b92aaaa97d8900591d999209d951c019e825e13
Ilias Tsitsimpis <iliastsi@hotmail.com>
2012-10-17 15:18:38 +0300

    Implement concuerrent testsuite

--- testsuite/runtests.sh
+++ testsuite/runtests.sh
@@ -4,6 +4,68 @@ concuerror="../concuerror"
 results="./results"
 prevDir=`pwd`
 
+# Extract scenarios from the specified test
+# Args:
+#   $1   : Test
+runTest() {
+    local temp=(`echo $1 | sed -e 's/suites\/\(\w\+\)\/src\/\(\w\+\)\(\.erl\)\?/\1 \2/'`)
+    local suite="${temp[0]}"
+    local name="${temp[1]}"
+    if [ -d $test ]; then
+        # Our test is a multi module directory
+        local dir=$test
+        local mod="test"
+        local files=(`ls $dir/*.erl`)
+    else
+        # Our test is a single module file
+        local dir=${test%/*}
+        local mod=$name
+        local files=$test
+    fi
+    # Create a dir to save the results
+    mkdir -p $results/$suite
+    # Compile it
+    erlc -W0 -o $dir $dir/$mod.erl
+    # And extract scenarios from it
+    local scenarios="`erl -noinput -pa . -pa $dir -s scenarios extract $mod -s init stop`"
+    local line
+    for line in ${scenarios//\\n/ }; do
+        # Get function and preemption bound
+        unset temp
+        temp=(`echo $line | sed -e 's/{\w\+,\(\w\+\),\(\w\+\)}/\1 \2/'`)
+        # And run the test
+        runScenario "$suite" "$name" "$mod" "${temp[0]}" "${temp[1]}" "${files[@]}" &
+    done
+    wait
+}
+
+# Run the specified scenario and print the results
+# Args:
+#   $1   : Suite
+#   $2   : Name
+#   $3   : Module
+#   $4   : Fun
+#   $5   : Preb
+#   Rest : Files
+runScenario() {
+    # Run concuerror
+    $concuerror --target $3 $4 --files "${@:6}" \
+        --output $results/$1/$2-$4-$5.txt \
+        --preb $5 --quiet
+    diff -I '<[0-9]\+\.[0-9]\+\.[0-9]\+>' \
+        -I '#Ref<[0-9\.]\+>' \
+        -uw suites/$1/results/$2-$4-$5.txt \
+        $results/$1/$2-$4-$5.txt &> /dev/null
+    if [ $? -eq 0 ]; then
+        printf "%-10s %-20s %-35s %-5s  \033[01;32mok\033[00m\n" \
+            "$1" "$2" "$4" "$5"
+    else
+        printf "%-10s %-20s %-35s %-5s  \033[01;31mfailed\033[00m\n" \
+            "$1" "$2" "$4" "$5"
+    fi
+}
+
+
 # Cleanup temp files
 cd `dirname $0`
 find . -name '*.beam' -exec rm {} \;
@@ -22,50 +84,11 @@ fi
 
 # For every test do
 for test in "${tests[@]}"; do
-    unset files
-    unset temp
-    temp=(`echo $test | sed -e 's/suites\/\(\w\+\)\/src\/\(\w\+\)\(\.erl\)\?/\1 \2/'`)
-    suite="${temp[0]}"
-    name="${temp[1]}"
-    if [ -d $test ]; then
-        # Our test is a multi module directory
-        dir=$test
-        mod="test"
-        files=(`ls $dir/*.erl`)
-    else
-        # Our test is a single module file
-        dir=${test%/*}
-        mod=$name
-        files=$test
-    fi
-    # Create a dir to save the results
-    mkdir -p $results/$suite
-    # Compile it
-    erlc -W0 -o $dir $dir/$mod.erl
-    # And extract scenarios from it
-    erl -noinput -pa . -pa $dir -s scenarios extract $mod -s init stop | \
-    while read line; do
-        # Get function and preemption bound
-        unset temp
-        temp=(`echo $line | sed -e 's/{\w\+,\(\w\+\),\(\w\+\)}/\1 \2/'`)
-        fun="${temp[0]}"
-        preb="${temp[1]}"
-        printf "Running test %s-%s (%s, %s).." $suite $name $fun $preb
-        # And run concuerror
-        $concuerror --target $mod $fun --files "${files[@]}" \
-            --output $results/$suite/$name-$fun-$preb.txt \
-            --preb $preb --quiet
-        diff -I '<[0-9]\+\.[0-9]\+\.[0-9]\+>' \
-            -I '#Ref<[0-9\.]\+>' \
-            -uw suites/$suite/results/$name-$fun-$preb.txt \
-            $results/$suite/$name-$fun-$preb.txt &> /dev/null
-        if [ $? -eq 0 ]; then
-            printf "\033[01;32mok\033[00m\n"
-        else
-            printf "\033[01;31mfailed\033[00m\n"
-        fi
-    done
+    runTest "$test" &
 done
+
+# Wait for all processes to finish
+wait
 
 # Cleanup temp files
 find . -name '*.beam' -exec rm {} \;
