1ba8570ffaa709c3a786f8eb0ee22a2f9efc1a5c
Ilias Tsitsimpis <iliastsi@hotmail.com>
2012-10-18 17:20:38 +0300

    Python parallel

--- testsuite/foo.py
+++ testsuite/foo.py
@@ -4,13 +4,13 @@ import sys
 import os
 import glob
 import subprocess
-from multiprocessing import Process, Lock
+from multiprocessing import Process, Lock, BoundedSemaphore
 
 
 #---------------------------------------------------------------------
 # Extract scenarios from the specified test
 
-def runTest(test):
+def runTest(sema, test):
     global dirname
     global results
     # test has the format of '.*/suites/<suite_name>/src/<test_name>(.erl)?'
@@ -38,20 +38,29 @@ def runTest(test):
     pout = subprocess.Popen(
             ["erl -noinput -pa %s -pa %s -s scenarios extract %s -s init stop"
             % (dirname, dirn, modn)], stdout=subprocess.PIPE, shell=True)
+    procS = []
     for scenario in pout.stdout:
         # scenario has the format of {<mod_name>,<func_name>,<preb>}\n
         scen = scenario.strip("{}\n").split(",")
         # And run the test
-        runScenario(suite, name, modn, scen[1], scen[2], files)
+        p = Process(target=runScenario,
+                args=(sema, suite, name, modn, scen[1], scen[2], files))
+        p.start()
+        procS.append(p)
     pout.stdout.close()
+    # Wait
+    for p in procS:
+        p.join()
 
 #---------------------------------------------------------------------
 # Run the specified scenario and print the results
 
-def runScenario(suite, name, modn, funn, preb, files):
+def runScenario(sema, suite, name, modn, funn, preb, files):
     global concuerror
     global results
     global dirname
+    IGNORE = open('/dev/null', 'w')
+    sema.acquire()
     # Run concuerror
     os.system("%s --target %s %s --files %s --output %s/%s/%s-%s-%s.txt --preb %s --quiet"
             % (concuerror, modn, funn, ' '.join(files), results, suite, name,
@@ -62,13 +71,14 @@ def runScenario(suite, name, modn, funn, preb, files):
                                "-uw %s/suites/%s/results/%s-%s-%s.txt " +
                                "%s/%s/%s-%s-%s.txt") % \
                               (dirname, suite, name, funn, preb, results, suite, name, funn, preb),
-                              shell=True, stdout=open('/dev/null', 'w'), stderr=subprocess.STDOUT)
+                              shell=True, stdout=IGNORE, stderr=IGNORE)
         print "%-10s %-20s %-40s  \033[01;32mok\033[00m" % \
                 (suite, name, "("+funn+", "+preb+")")
     except subprocess.CalledProcessError:
         print "%-10s %-20s %-40s  \033[01;31mfailed\033[00m" % \
                 (suite, name, "("+funn+", "+preb+")")
-
+    sema.release()
+    IGNORE.close()
 
 #---------------------------------------------------------------------
 # Main program
@@ -94,8 +104,15 @@ else:
     tests = glob.glob(dirname + "/suites/*/src/*")
 
 # For every test do
+procT = []
+sema = BoundedSemaphore(2)
 for test in tests:
-    runTest(test)
+    p = Process(target=runTest, args=(sema, test))
+    p.start()
+    procT.append(p)
+# Wait
+for p in procT:
+    p.join()
 
 # Cleanup temp files
 os.system("find %s -name '*.beam' -exec rm {} \;" % dirname)
